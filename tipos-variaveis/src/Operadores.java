41488f5cce86bbbdeb34957cfadd97223cb0c7f7
devlucascalado <devlucascalado@gmail.com>
2024-01-18 21:47:21 -0300

    operadores e variaveis

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,17 @@
+public class Operadores {
+    public static void main(String[] args) {
+
+        double soma = 10.5 + 15.7; // operador soma " + "
+        int subtracao = 15 - 5; //operador subtração " - "
+        int multiplicacao = 15 * 3; //operador multiplicação " * "
+        int divisao = 15 / 5 ; // operador divisão " / "
+        int modulo = 18 % 3; // operador módulo(resto da divisão) " % "
+        boolean verdade = true;
+        int numero = 15;
+        //numero++; //numero + 1
+
+        System.out.println(++ numero); // ++ operador para incremento de valor (adiciona 1 ao valor)
+        System.out.println(-- numero); // -- operador para decremento de valor (diminue 1 ao valor)
+        System.out.println(!verdade); // ! operador que nega o valor da variavel(inverte boolean)
+     }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // tipos de variaveis da linguagem java
+        String primeiroNome = "Lucas"; //variável para cadeias de caracteres
+
+        byte idade = 24; // variavel para numeros que recebe valores que vão de -128 a 127
+
+        float altura = 1.70f; //variavel que recebe números com ponto flutuante, deve terminar com a letra f;
+
+        short anoNascimento = 1999; // variavel para numeros que recebe valores que vão de -32k+- até +-32k
+
+        double dinheiro = 1000.50; // variavel para numeros com ponto flutuante
+
+        long bilhao = 1000000000l; // variavel para números muito grandes, deve terminar com a letra l;
+
+        final String sobrenome = "Vieira"; //para declarar uma constante, devo utilizar o escopo = final tipoVariavel nomeVariavel atribuição
+        
+        boolean verdade = true; //variavel lógica que recebe valores true ou false 
     }
 }
